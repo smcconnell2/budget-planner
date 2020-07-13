f3acc5e319060b0efb0b7bd7871dcbe42cf5bbc5
smcconnell2 <50210982+smcconnell2@users.noreply.github.com>
2020-07-13 12:59:59 -0700

    Getting initial project setup

---
+++ src/main/RunProgram.java
@@ -0,0 +1,6 @@
+package main;
+
+public class RunProgram {
+	public RunProgram() {
+	}
+}
---
+++ src/utils/ScannerClass.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.util.Currency;
+import java.util.Scanner;
+
+/**
+ * 
+ * @author steve
+ *Creates a static Scanner object to read in user input and to handle
+ *basic user input checks to ensure proper type of information is 
+ *entered by the user.
+ */
+public class ScannerClass {
+	public static Scanner scan;
+	public Currency test;
+	
+	public static String readString() {
+		
+		return "";
+	}
+	
+	public static char readChar() {
+		
+		return 'e';
+	}
+	
+	public static int readInt() {
+		
+		return 0;
+	}
+	
+	public static double readDouble() {
+		
+		return 0.0;
+	}
+}
